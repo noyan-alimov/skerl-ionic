d9acfaaf0cd48b5982efc11176af6fccd0676bbb
Noyan <noyan.alimov@gmail.com>
2021-04-22 16:44:16 +0100

    cleaned up template

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,4 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
@@ -16,7 +14,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <h1>Tab 1 Page</h1>
       </IonContent>
     </IonPage>
   );
